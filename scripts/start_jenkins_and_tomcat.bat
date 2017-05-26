98477782c3a68646a54c2d9827cac5685af0def1
vivganes <caliberoviv@gmail.com>
2017-05-26 13:35:11 +0530

    Add documentation

--- scripts/start_jenkins_and_tomcat.bat
+++
@@ -1,3 +0,0 @@
-#start java -jar C:\Users\Vivek\source-codes\jenkins\war\target\jenkins.war
-C:\Users\Vivek\source-codes\tomcat-prod\bin\startup.bat
-C:\Users\Vivek\source-codes\tomcat-staging\bin\startup.bat
