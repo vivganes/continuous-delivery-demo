4eb3feab9504f89e5d53b1260b5e71cfeb9e6ee6
ampyard-vivek-ganesan <vivek.ganesan@ampyard.com>
2019-04-28 14:11:06 +0530

    user provider change

--- src/main/java/com/vivekganesan/cddemo/featuretoggles/FeatureToggleConfig.java
+++ src/main/java/com/vivekganesan/cddemo/featuretoggles/FeatureToggleConfig.java
@@ -7,6 +7,8 @@ import org.togglz.core.Feature;
 import org.togglz.core.manager.TogglzConfig;
 import org.togglz.core.repository.StateRepository;
 import org.togglz.core.repository.file.FileBasedStateRepository;
+import org.togglz.core.user.FeatureUser;
+import org.togglz.core.user.SimpleFeatureUser;
 import org.togglz.core.user.UserProvider;
 import org.togglz.servlet.user.ServletUserProvider;
 
@@ -24,6 +26,12 @@ public class FeatureToggleConfig implements TogglzConfig {
     }
 
     public UserProvider getUserProvider() {
-        return new ServletUserProvider("admin");
+        return new UserProvider() {
+            @Override
+
+            public FeatureUser getCurrentUser() {
+                return new SimpleFeatureUser("admin", true);
+            }
+        };
     }
 }
